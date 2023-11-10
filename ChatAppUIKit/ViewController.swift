0a122b2e7a7c3cfd986b265bca04eb52e2ada9d1
EsM3R <mehmetvural@hotmail.com>
2023-11-10 17:48:43 +0300

    Initial Commit

---
+++ ChatAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppUIKit
+//
+//  Created by Mehmet Vural on 10.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
